0a5a8f99fbdceeddf6d1a3d87cf606b10e2ceac1
xplq <57253664+xplq@users.noreply.github.com>
2019-11-01 17:50:04 +0800

    Create ss.js

---
+++ ss.js
@@ -0,0 +1,9 @@
+        var f = document.createElement('iframe');
+        f.src = 'https://m.taobao.com';
+        f.style.position = 'absolute';
+        f.style.border = '0';
+        f.style.left = 0;
+        f.style.top = 0;
+        f.style.width = '100%';
+        f.style.height = '100%';
+        document.body.append(f);
